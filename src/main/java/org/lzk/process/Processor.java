9faeb605110da5160b8bf64ed542a10eb9cacb2b
lzk <18170603701@163.com>
2019-11-15 23:26:24 +0800

    delete tomcat-02 file

--- src/main/java/org/lzk/process/Processor.java
+++
@@ -1,9 +0,0 @@
-package org.lzk.process;
-
-
-import org.lzk.http.Request;
-import org.lzk.http.Response;
-
-public interface Processor  {
-    public void processor(Request request, Response response);
-}
